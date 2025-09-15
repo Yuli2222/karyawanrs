03ae20429f20bb3d5fecfd64650bca59fe581953
Yuli2222 <ylndaaa6@gmail.com>
2025-09-16 00:18:40 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,12 +1,14 @@
 <?php
-$host = "localhost";
-$user = "root"; // contoh username default XAMPP
-$pass = ""; // contoh password default XAMPP
-$db   = "karyawanrs";
+$host = "sql100.infinityfree.com";   // hostname dari InfinityFree
+$user = "if0_39947784";              // username MySQL kamu
+$pass = "YuliAnda22025";             // password MySQL kamu
+$db   = "if0_39947784_karyawan_rs";  // nama database
 
 $conn = new mysqli($host, $user, $pass, $db);
 
 if ($conn->connect_error) {
     die("Koneksi gagal: " . $conn->connect_error);
+} else {
+    echo "Koneksi berhasil!";
 }
 ?>
